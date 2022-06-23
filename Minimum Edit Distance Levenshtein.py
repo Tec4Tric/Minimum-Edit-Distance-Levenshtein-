69df445f21b6623f9dd05c2373fe30af0353e831
Sayan De <sayande200@gmail.com>
2022-06-23 13:36:49 +0530

    Add files via upload

---
+++ Minimum Edit Distance Levenshtein.py
@@ -0,0 +1,17 @@
+# Minimum Edit Distance using Levenshtein formula
+# Algorithm by Dan Jurafsky
+# Implemented by Sayan De
+
+str1="Intension"
+str2="Execution"
+
+def med(str1,str2,len_str1,len_str2):
+	if len_str1 == 0:
+		return len_str2
+	if len_str2 == 0:
+		return len_str1
+	if str1[len_str1-1] == str2[len_str2-1]:
+		return med(str1,str2,len_str1-1,len_str2-1)
+	return min(med(str1,str2,len_str1,len_str2-1) + 1, med(str1,str2,len_str1-1,len_str2) + 1, med(str1,str2,len_str1-1,len_str2-1) + (2 if str1[len_str1] !=str2[len_str1] else 0))
+
+print(med(str1,str2,len(str1),len(str2)))
